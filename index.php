c639eb93df4721f54e8b23b20c8812ebff39f441
Joao Souza <mariiassilvana@gmail.com>
2020-01-26 09:39:54 -0300

    Resolvi os conflitos echos

--- index.php
+++ index.php
@@ -3,11 +3,6 @@
 <<<<<<< HEAD
     echo "Ola Mundo 456";
 
-    echo "Olá Udemy";
-=======
-    echo "Ola Mundo123";
-
-    echo "OLA MR";
->>>>>>> 89c21aecb9bb41900b5e3ad7aeb9415f9c1010ad
+    echo "Olá Udemy e MrSniper!";
 
 ?>
